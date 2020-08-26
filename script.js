b49d9b7e5197a926ce3758c94ecb664de2b01272
Rodrigo Ramos <rodrigoramos@Mac-mini-de-Rodrigo.local>
2020-08-26 10:37:36 -0300

    Getting some bug correct

--- script.js
+++ script.js
@@ -349,16 +349,16 @@ function printUsers(qty){
 
   section.innerHTML =  auxUsers;
 
-  document.getElementById('download').addEventListener('click', ()=>{
-    let pdfFile = document.getElementById('section')
-    var opt = {
-      margin: 0,
-      filename: 'myfile.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
-    html2pdf().from(pdfFile).set(opt).save()
-  })
+  // document.getElementById('download').addEventListener('click', ()=>{
+  //   let pdfFile = document.getElementById('section')
+  //   var opt = {
+  //     margin: 0,
+  //     filename: 'myfile.pdf',
+  //     image: { type: 'jpeg', quality: 0.98 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+  //   };
+  //   html2pdf().from(pdfFile).set(opt).save()
+  // })
 
 })}
