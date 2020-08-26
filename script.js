a5fe0d42fcdb1e0809571d5e4e62d5bb44de13c5
Rodrigo Ramos <rodrigoramos@MacBook-Pro-de-Rodrigo.local>
2020-08-26 00:47:22 -0300

    Finished the crud with google-sheets and google forms

--- script.js
+++ script.js
@@ -4,25 +4,258 @@ var section = document.getElementById("section")
 function handleForm(){
   window.location='/'
 }
+function closeModal(){
+  document.getElementById('edit-modal').style.display = 'none'
+}
+function handleDeleteUser(e){
+// Usable variables:
+  
+let result;
+var paramsQty = 5
+var auxUsers = []
+// Objects:
+
+  function usersDb(
+    date, name,  username, email, editUrl)
+      {
+      this.date = date,
+      this.name = name,
+      this.username = username,
+      this.email = email,
+      this.editUrl = editUrl
+    }
+  
+  
+      const arrayUsers =  fetch(
+          "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
+        )
+          .then(function(res){ 
+            return res.json()})
+          .then(function(jsonRes){
+            result = jsonRes.feed.entry.map((row)=> row.content["$t"])  
+         
+  // Variables which depends from the response:
+  var users = []
+  var objSize = Object
+  .keys(result)
+  .length
+  
+  
+  objSize = objSize/paramsQty
+  
+  
+ 
+  
+  
+  
+  // Including the api response into the object:
+  for(i=0 ; i < objSize ; i++){
+      count = i*paramsQty
+     users[i] = new usersDb(
+        result[count],
+        result[count+1],
+        result[count+2],
+        result[count+3],
+        result[count+4]
+     )}
+  
+
+  document.getElementById('edit-modal').style.opacity = 0
+
+
+  let newURL = `${users[e].editUrl.substring(0,29)}/u/0/${users[e].editUrl.substring(30,91)}formResponse${users[e].editUrl.substring(99)}`
+  
+  
+  document.getElementById('edit-modal').innerHTML = `<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"      
+  onload="if(submitted) {window.location='./#user-${e}'}"></iframe> 
+    <form name='shouldDeletForms' id="formId" onsubmit="handleForm()" 
+    action="${ newURL }"
+    target="hidden_iframe"
+    method="POST"
+    id="mG61Hd">
+    <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+        <input class="input" type="text" placeholder="Text input" name="entry.1696280753" value="Deleted" required>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Username</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Text input" name="entry.954027946" value=" ${users[e].username}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+        </span>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="email" placeholder="Email input" name="entry.829195258" value=" ${users[e].email}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-exclamation-triangle"></i>
+        </span>
+        </div>
+    </div>
+
+    <div class="field is-grouped">
+        <div class="control">
+        <button type="submit" class="button is-link">Salvar</button>
+        </div>
+        <div class="control">
+        <button class="button is-link is-light" onclick="closeModal(e)" >Cancelar</button>
+        </div>
+    </div>
+  </form>`
+
+
+document.shouldDeletForms.submit()
+window.location='./'
+})}
+function handleEditForms(e){
+// Usable variables:
+  
+let result;
+var paramsQty = 5
+var auxUsers = []
+// Objects:
+
+  function usersDb(
+    date, name,  username, email, editUrl)
+      {
+      this.date = date,
+      this.name = name,
+      this.username = username,
+      this.email = email,
+      this.editUrl = editUrl
+    }
+  
+  
+      const arrayUsers =  fetch(
+          "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
+        )
+          .then(function(res){ 
+            return res.json()})
+          .then(function(jsonRes){
+            result = jsonRes.feed.entry.map((row)=> row.content["$t"])  
+         
+  // Variables which depends from the response:
+  var users = []
+  var objSize = Object
+  .keys(result)
+  .length
+  
+  
+  objSize = objSize/paramsQty
+  
+  
+ 
+  
+  
+  
+  // Including the api response into the object:
+  for(i=0 ; i < objSize ; i++){
+      count = i*paramsQty
+     users[i] = new usersDb(
+        result[count],
+        result[count+1],
+        result[count+2],
+        result[count+3],
+        result[count+4]
+     )}
+  
+
+  document.getElementById('edit-modal').style.display = 'flex'
+
+
+  let newURL = `${users[e].editUrl.substring(0,29)}/u/0/${users[e].editUrl.substring(30,91)}formResponse${users[e].editUrl.substring(99)}`
+  
+  
+  document.getElementById('edit-modal').innerHTML = `<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"      
+  onload="if(submitted) {window.location='./#user-${e}'}"></iframe> 
+    <form id="formId" onsubmit="handleForm()" 
+    action="${ newURL }"
+    target="hidden_iframe"
+    method="POST"
+    id="mG61Hd">
+    <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+        <input class="input" type="text" placeholder="Text input" name="entry.1696280753" value=" ${users[e].name}" required>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Username</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Text input" name="entry.954027946" value=" ${users[e].username}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+        </span>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="email" placeholder="Email input" name="entry.829195258" value=" ${users[e].email}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-exclamation-triangle"></i>
+        </span>
+        </div>
+    </div>
+
+    <div class="field is-grouped">
+        <div class="control">
+        <button type="submit" class="button is-link">Salvar</button>
+        </div>
+        <div class="control">
+        <button class="button is-link is-light" onclick="closeModal(e)" >Cancelar</button>
+        </div>
+    </div>
+  </form>`
+
+
+  document.getElementById('formId').addEventListener('focusout', ()=>{
+    closeModal()
+  })
+})}
+
 function loadDb(){
 // Usable variables:
   
   let result;
-  var paramsQty = 4
+  var paramsQty = 5
   var auxUsers = []
 // Objects:
 
 function usersDb(
-    name, username, email)
+  date, name,  username, email, editUrl)
     {
+    this.date = date,
     this.name = name,
     this.username = username,
-    this.email = email
+    this.email = email,
+    this.editUrl = editUrl
   }
 
 
     const arrayUsers =  fetch(
-        "https://spreadsheets.google.com/feeds/cells/1-80ccEX-eT_W9lFoD4gpirBtjCk5s0gzw6Pa83uHCXY/1/public/full?alt=json"
+        "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
       )
         .then(function(res){ 
           return res.json()})
@@ -49,12 +282,18 @@ for(i=0 ; i < objSize ; i++){
    users[i] = new usersDb(
       result[count],
       result[count+1],
-      result[count+2]
+      result[count+2],
+      result[count+3],
+      result[count+4]
    )}
 
 
 // Function which print users' users on the screen:
+
 function printUsers(qty){
+  // Function which handle the edit forms url
+
+
     while (   
       document
       .getElementById('section')
@@ -68,8 +307,8 @@ function printUsers(qty){
     }
     for(i=1 ; i < qty ; i++){
 
-
-          auxUsers =  auxUsers + `<div class="card" style=" margin: 20px; ">
+      
+          if(users[i].name!='Deleted'){auxUsers =  `<div id="user-${i}" class="card" style=" margin: 20px; ">
           <header class="card-header">
             <p class="card-header-title">
             ${users[i].name}
@@ -89,15 +328,20 @@ function printUsers(qty){
             </div>
           </div>
           <footer class="card-footer">
-            <a href="#" class="card-footer-item">Save</a>
-            <a href="#" class="card-footer-item">Edit</a>
-            <a href="#" class="card-footer-item">Delete</a>
+            <a href="#user-${i}" class="card-footer-item" 
+            onclick="handleEditForms(${i})" >Editar</a>
+            <a href="#user-${i}" 
+            onclick="handleDeleteUser(${i})" class="card-footer-item">Deletar</a>
           </footer>
-          </div>`
+          </div>` + auxUsers }
       
     }
+
+
 }
+
   printUsers(users.length)
+  
 
   section.innerHTML =  auxUsers;
 
