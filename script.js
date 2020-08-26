9095519fcde228f4665b2ae210517b1861cd008e
Rodrigo Ramos <rodrigoramos@Mac-mini-de-Rodrigo.local>
2020-08-26 10:29:10 -0300

    correcting some bugs

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function handleDeleteUser(e){
 // Usable variables:
   
 let result;
-var paramsQty = 5
-var auxUsers = []
+var paramsQty = pages
+
 // Objects:
 
   function usersDb(
@@ -26,120 +26,7 @@ var auxUsers = []
     }
   
   
-      const arrayUsers =  fetch(
-          "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
-        )
-          .then(function(res){ 
-            return res.json()})
-          .then(function(jsonRes){
-            result = jsonRes.feed.entry.map((row)=> row.content["$t"])  
-         
-  // Variables which depends from the response:
-  var users = []
-  var objSize = Object
-  .keys(result)
-  .length
-  
-  
-  objSize = objSize/paramsQty
-  
-  
- 
-  
-  
-  
-  // Including the api response into the object:
-  for(i=0 ; i < objSize ; i++){
-      count = i*paramsQty
-     users[i] = new usersDb(
-        result[count],
-        result[count+1],
-        result[count+2],
-        result[count+3],
-        result[count+4]
-     )}
-  
-
-  document.getElementById('edit-modal').style.opacity = 0
-
-
-  let newURL = `${users[e].editUrl.substring(0,29)}/u/0/${users[e].editUrl.substring(30,91)}formResponse${users[e].editUrl.substring(99)}`
-  
-  
-  document.getElementById('edit-modal').innerHTML = `<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"      
-  onload="if(submitted) {window.location='./#user-${e}'}"></iframe> 
-    <form name='shouldDeletForms' id="formId" onsubmit="handleForm()" 
-    action="${ newURL }"
-    target="hidden_iframe"
-    method="POST"
-    id="mG61Hd">
-    <div class="field">
-        <label class="label">Name</label>
-        <div class="control">
-        <input class="input" type="text" placeholder="Text input" name="entry.1696280753" value="Deleted" required>
-        </div>
-    </div>
-    
-    <div class="field">
-        <label class="label">Username</label>
-        <div class="control has-icons-left has-icons-right">
-        <input class="input" type="text" placeholder="Text input" name="entry.954027946" value=" ${users[e].username}" required>
-        <span class="icon is-small is-left">
-            <i class="fas fa-user"></i>
-        </span>
-        <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-        </span>
-        </div>
-    </div>
-    
-    <div class="field">
-        <label class="label">Email</label>
-        <div class="control has-icons-left has-icons-right">
-        <input class="input" type="email" placeholder="Email input" name="entry.829195258" value=" ${users[e].email}" required>
-        <span class="icon is-small is-left">
-            <i class="fas fa-envelope"></i>
-        </span>
-        <span class="icon is-small is-right">
-            <i class="fas fa-exclamation-triangle"></i>
-        </span>
-        </div>
-    </div>
-
-    <div class="field is-grouped">
-        <div class="control">
-        <button type="submit" class="button is-link">Salvar</button>
-        </div>
-        <div class="control">
-        <button class="button is-link is-light" onclick="closeModal(e)" >Cancelar</button>
-        </div>
-    </div>
-  </form>`
-
-
-document.shouldDeletForms.submit()
-window.location='./'
-})}
-function handleEditForms(e){
-// Usable variables:
-  
-let result;
-var paramsQty = 5
-var auxUsers = []
-// Objects:
-
-  function usersDb(
-    date, name,  username, email, editUrl)
-      {
-      this.date = date,
-      this.name = name,
-      this.username = username,
-      this.email = email,
-      this.editUrl = editUrl
-    }
-  
-  
-      const arrayUsers =  fetch(
+      fetch(
           "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
         )
           .then(function(res){ 
@@ -174,7 +61,124 @@ var auxUsers = []
   
 
   document.getElementById('edit-modal').style.display = 'flex'
+  document.getElementById('edit-modal').style.opacity = 0
 
+
+  let newURL = `${users[e].editUrl.substring(0,29)}/u/0/${users[e].editUrl.substring(30,91)}formResponse${users[e].editUrl.substring(99)}`
+  
+  
+  document.getElementById('edit-modal').innerHTML = `<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"      
+  onload="if(submitted) {window.location='./#user-${e}'}"></iframe> 
+    <form name='shouldDeletForms' id="formId" onsubmit="handleForm()" 
+    action="${ newURL }"
+    target="hidden_iframe"
+    method="POST"
+    id="mG61Hd">
+    <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+        <input class="input" type="text" placeholder="Exemplo: José Nascimento" name="entry.1696280753" value="Deleted" required>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Username</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Exemplo: jsnasc" name="entry.954027946" value=" ${users[e].username}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-user"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+        </span>
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+        <input class="input" type="email" placeholder="Exemplo: joaonasc@email.com" name="entry.829195258" value=" ${users[e].email}" required>
+        <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+        </span>
+        <span class="icon is-small is-right">
+            <i class="fas fa-exclamation-triangle"></i>
+        </span>
+        </div>
+    </div>
+
+    <div class="field is-grouped">
+        <div class="control">
+        <button type="submit" class="button is-link">Salvar</button>
+        </div>
+        <div class="control">
+        <button class="button is-link is-light" onclick="closeModal(e)" >Cancelar</button>
+        </div>
+    </div>
+  </form>`
+
+setTimeout(() => {
+document.shouldDeletForms.submit()
+handleForm()
+}, 750);
+})}
+function handleEditForms(e){
+// Usable variables:
+  
+let result;
+var paramsQty = pages
+
+// Objects:
+
+  function usersDb(
+    date, name,  username, email, editUrl)
+      {
+      this.date = date,
+      this.name = name,
+      this.username = username,
+      this.email = email,
+      this.editUrl = editUrl
+    }
+  
+  
+      const arrayUsers =  fetch(
+          "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
+        )
+          .then(function(res){ 
+            return res.json()})
+          .then(function(jsonRes){
+            result = jsonRes.feed.entry.map((row)=> row.content["$t"])  
+         
+  // Variables which depends from the response:
+  var users = []
+  var objSize = Object
+  .keys(result)
+  .length
+  
+  
+  objSize = objSize/paramsQty
+  
+  
+ 
+  
+  
+  
+  // Including the api response into the object:
+  for(i=0 ; i < objSize ; i++){
+      count = i*paramsQty
+     users[i] = new usersDb(
+        result[count],
+        result[count+1],
+        result[count+2],
+        result[count+3],
+        result[count+4]
+     )}
+  
+setTimeout(() => {
+  
+  document.getElementById('edit-modal').style.display = 'flex'
+
+}, 750);
 
   let newURL = `${users[e].editUrl.substring(0,29)}/u/0/${users[e].editUrl.substring(30,91)}formResponse${users[e].editUrl.substring(99)}`
   
@@ -189,14 +193,14 @@ var auxUsers = []
     <div class="field">
         <label class="label">Name</label>
         <div class="control">
-        <input class="input" type="text" placeholder="Text input" name="entry.1696280753" value=" ${users[e].name}" required>
+        <input class="input" type="text" placeholder="Exemplo: José Nascimento" name="entry.1696280753" value=" ${users[e].name}" required>
         </div>
     </div>
     
     <div class="field">
         <label class="label">Username</label>
         <div class="control has-icons-left has-icons-right">
-        <input class="input" type="text" placeholder="Text input" name="entry.954027946" value=" ${users[e].username}" required>
+        <input class="input" type="text" placeholder="Exemplo: jsnasc" name="entry.954027946" value=" ${users[e].username}" required>
         <span class="icon is-small is-left">
             <i class="fas fa-user"></i>
         </span>
@@ -209,7 +213,7 @@ var auxUsers = []
     <div class="field">
         <label class="label">Email</label>
         <div class="control has-icons-left has-icons-right">
-        <input class="input" type="email" placeholder="Email input" name="entry.829195258" value=" ${users[e].email}" required>
+        <input class="input" type="email" placeholder="Exemplo: joaonasc@email.com" name="entry.829195258" value=" ${users[e].email}" required>
         <span class="icon is-small is-left">
             <i class="fas fa-envelope"></i>
         </span>
@@ -253,8 +257,7 @@ function usersDb(
     this.editUrl = editUrl
   }
 
-
-    const arrayUsers =  fetch(
+fetch(
         "https://spreadsheets.google.com/feeds/cells/1WxMXjI2m_fEQ0zZ7pTpLwcJGEixCrM3N7r7s-q4skgE/1/public/full?alt=json"
       )
         .then(function(res){ 
